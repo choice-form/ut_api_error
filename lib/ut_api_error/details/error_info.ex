062a8be43031814b3c605e2c59dc4bb65d3143da
David Chen <david.chen@choiceform.com>
2025-01-02 15:53:00 +0800

    feat: make ErrorInfo reason atom or string

--- lib/ut_api_error/details/error_info.ex
+++ lib/ut_api_error/details/error_info.ex
@@ -2,7 +2,7 @@ defmodule UtApiError.Details.ErrorInfo do
   @moduledoc """
   错误原因的结构体
 
-  * `reason` - 错误原因，在 `domain` 下唯一
+  * `reason` - 错误原因，在 `domain` 下唯一。在转换成 JSON 时它会转换成全大写的字符串
   * `domain` - reason 的逻辑分组
   * `metadata` - 额外的结构化数据，方便程序处理
   """
@@ -10,7 +10,7 @@ defmodule UtApiError.Details.ErrorInfo do
   defstruct [:reason, :domain, :metadata]
 
   @type t :: %__MODULE__{
-          reason: String.t(),
+          reason: atom() | String.t(),
           domain: String.t() | nil,
           metadata: map() | nil
         }
@@ -19,7 +19,7 @@ defmodule UtApiError.Details.ErrorInfo do
     def encode(data, opts) do
       %{
         "$type": "ErrorInfo",
-        reason: data.reason |> String.upcase(),
+        reason: data.reason |> to_string() |> String.upcase(),
         domain: data.domain,
         metadata: data.metadata
       }
