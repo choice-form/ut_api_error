b2f9aedb6d3035eae8b00622afe917b8875f278b
Kenton Wang <smartepsh@gmail.com>
2025-07-14 19:26:18 +0800

    chore: bump version to v0.3.0

--- mix.exs
+++ mix.exs
@@ -1,10 +1,12 @@
 defmodule UtApiError.MixProject do
   use Mix.Project
 
+  @version "0.3.0"
+
   def project do
     [
       app: :ut_api_error,
-      version: "0.2.0",
+      version: @version,
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps()
